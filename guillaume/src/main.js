8773076046400e30ac2e40a280dfcb70836ade29
Leveque <guillaume.leveque@epitech.eu>
2021-03-21 21:34:00 +0100

    21/03

--- guillaume/src/main.js
+++ guillaume/src/main.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './style/modal.css';
 import {Link, Route, Switch} from "react-router-dom";
 import Projet from './projet.js';
 import Linkedin from "./img/linkedin.png";
@@ -7,9 +8,45 @@ import {
 } from "react-share";
 import { Document, Page } from 'react-pdf';
 import Pdf from "./img/mail-pro.pdf";
+import Modal from './modal.js';
+import React, { Component } from "react";
+import emailjs from "emailjs-com";
+import{ init } from 'emailjs-com';
+init("user_tkxypSsomPEpESZcwelDY");
 
-function Main() {
-  return (
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+  }
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+  sendEmail =(e) => {
+    e.preventDefault();
+    alert("test");
+
+    emailjs.sendForm('service_opq364p', 'template_eb9f3pp', e.target, 'user_tkxypSsomPEpESZcwelDY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
+  render() {
+    return (
+      <main>
     <div class="main_index">
       <div class="context">
         <h1>Guillaume Leveque</h1>
@@ -24,7 +61,20 @@ function Main() {
               <a href = {Pdf} target = "_blank">View cv</a>
             </div>
             <div class="button_mail">
-              <p>levequegui@gmail</p>
+              <Modal show={this.state.show} handleClose={this.hideModal}>
+                <div class="container_modal">
+                  <form onSubmit={this.sendEmail}>
+                    <input id="input_contact" type="text" name="name" placeholder="name"></input>
+                    <input id="input_contact" type="email" name="email" placeholder="email"></input>
+                    <input id="input_contact" type="text" name="subject" placeholder="sujet"></input>
+                    <textarea id="input_contact" name="message" placeholder="Votre message" rows="10" cols="30"></textarea>
+                    <input id="input_contact_submit" type="submit" value="Envoyer"></input>
+                  </form>
+                </div>
+              </Modal>
+              <button type="button" id="button_contact" onClick={this.showModal}>
+                Contact
+              </button>
             </div>
             <div class="button_projet">
                 <p><Link to="/Projet">Projet</Link></p>
@@ -46,7 +96,10 @@ function Main() {
           </ul>
         </div>
     </div>
-  );
+      </main>
+    );
+  }
 }
 
-export default Main;
+
+export default Dashboard;
